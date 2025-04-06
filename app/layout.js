e416cf6aad917060c4c870d912aa609ada5bf9b6
Batman <electronicnoise185@gmail.com>
2025-04-06 00:09:28 -0400

    Add Google AdSense script to layout.js and create ads.txt

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Script from 'next/script'; // Importar Script de next/script
 
-// Configurar la fuente Inter
 const inter = Inter({
   subsets: ['latin'],
   weight: ['400', '600', '700'],
@@ -18,6 +18,9 @@ export default function RootLayout({ children }) {
     <html lang="en" className={inter.className}>
       <head>
         <link rel="icon" href="/favicon.ico?v=3" />
+        {/* Añadir el script de AdSense */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6479199056016392"crossorigin="anonymous">
+        </script>
       </head>
       <body>{children}</body>
     </html>
